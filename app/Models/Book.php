94ced609c5c467af7a9b254d37eceff9a7be4ebf
Khaled <khalegjr@gmail.com>
2023-01-29 22:13:53 -0300

    Implementa relacionamento entre as tabelas

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Notifications\Notifiable;
 
 class Book extends Model
@@ -20,4 +21,9 @@ class Book extends Model
         'author',
         'state',
     ];
+
+    public function genres() : BelongsToMany
+    {
+        return $this->belongsToMany(Genre::class);
+    }
 }
